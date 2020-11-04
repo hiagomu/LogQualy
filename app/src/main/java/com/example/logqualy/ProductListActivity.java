6c362df90fee244fc5880fb7c37ee13015856088
hiago <hiagomuuu@gmail.com>
2020-11-04 16:50:52 -0300

    -Add Logout

--- app/src/main/java/com/example/logqualy/ProductListActivity.java
+++ app/src/main/java/com/example/logqualy/ProductListActivity.java
@@ -1,9 +1,13 @@
 package com.example.logqualy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -14,11 +18,46 @@ public class ProductListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list);
 
-        //FirebaseAuth.getInstance().signOut();
-
         if (FirebaseAuth.getInstance().getCurrentUser() == null) {
             Intent intent = new Intent(getApplicationContext(), MainActivity.class);
             startActivity(intent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        // Com if/else
+//        if(item.getItemId()==R.id.menu_item_logout){
+//            logOut();
+//            return true;
+//        } else {
+//            return super.onOptionsItemSelected(item);
+//        }
+        switch (item.getItemId()) {
+            case R.id.menu_item_logout:
+                logOut();
+                goToLoginAct();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void logOut() {
+        FirebaseAuth.getInstance().signOut();
+    }
+
+    private void goToLoginAct() {
+        Intent intent = new Intent(ProductListActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
